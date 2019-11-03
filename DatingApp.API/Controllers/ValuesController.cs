33aa99cf3dbdbe0b4d683954c171eafb0c7ec496
alberthernando <alberthe91@gmail.com>
2019-11-03 22:53:33 +1100

    git commit -m "AddedAuthenticationAnd Registration"

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
@@ -19,6 +21,7 @@ namespace DatingApp.API.Controllers
             _context = context;            
         }
 
+        //GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -26,6 +29,8 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
+        //GET api/values/id
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
