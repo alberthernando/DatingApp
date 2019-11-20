590e52d6a1f2e2565ff00dcf664f850da440b1d6
alberthernando <alberthe91@gmail.com>
2019-11-20 21:14:00 +1100

    git commit -m "End of Session 4"

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -2,6 +2,7 @@ namespace DatingApp.API.Dtos
 {
     public class UserForLoginDto
     {
+        [Required]
         public string Username { get; set; }
 
         public string Password { get; set; }        
